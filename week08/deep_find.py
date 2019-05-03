1dcbcd2707aada97c0b8e4f97b3e78f0697f6f3f
PreslavaKuzova <preslava.kuzova@gmail.com>
2019-05-04 00:09:07 +0300

    Code improvement and tests added

--- week08/deep_find.py
+++ week08/deep_find.py
@@ -8,8 +8,9 @@ graph = {'A': ['D', 'C', 'E'],
 
 from collections import deque
 class BFS:
-    def breadth_first_search(self, graph:dict, vertice,visited_nodes = set()):
+    def breadth_first_search(self, graph:dict, vertice, visited_nodes = []):
         nonvisited_nodes = list(graph.keys())
+        visited_nodes = []
 
         # This is needed, because for example the first key might not have any values
         for node in nonvisited_nodes:
@@ -20,11 +21,10 @@ class BFS:
             # of earlier visited nodes before the neighbors of more recently visited ones
             while len(queue) > 0:
                 new_node = queue.pop()
-                
                 if new_node in visited_nodes:
                     continue
             
-                visited_nodes.add(new_node)
+                visited_nodes.append(new_node)
                 if vertice in graph[new_node]:
                     return graph[new_node]
 
@@ -32,7 +32,7 @@ class BFS:
                 for n in adjacent_nodes:
                     if n not in visited_nodes:
                         queue.appendleft(n)
-
+        
         return False
 
 class DFS:
@@ -40,31 +40,32 @@ class DFS:
     def __init__(self):
         self.visited_nodes = []
         self.recursion_steps = []
+    
     # Pick any unvisited vertex adjacent to the current vertex, and check to see if this is the goal.
     # If not, recursively apply the depth-first search to that vertex, 
     # ignoring any vertices that have already been visited.
     def depth_first_search(self, graph: dict, vertice, visited_nodes=[]):
         nonvisited_nodes = list(graph.keys())
         
-        #this is needed, because for example the first key might not have any values
+        # This is needed, because for example the first key might not have any values
         for node in nonvisited_nodes:
             self.recursion_steps = []
             result = self.inner(node, graph, vertice)
             
             if result:  
-                #we get only the first execution return value of the recursion
+                # We get only the first execution return value of the recursion
                 return graph[self.recursion_steps[0]]
 
             nonvisited_nodes.remove(node)
         return False
 
-    #recursive function which finds the appearance of the key in the current value
+    # Recursive function which finds the appearance of the key in the current value
     def inner(self, node: str, graph: dict, vertice):
         if vertice in graph[node]:
                 self.recursion_steps.append(node)
                 return True
 
-        #as long as the key is not the current value, we check the values themselves for the same 
+        # As long as the key is not the current value, we check the values themselves for the same 
         self.visited_nodes.append(node)
         for adj_node in graph[node]:
             if adj_node not in self.visited_nodes:
@@ -80,8 +81,11 @@ def main():
 
     # Results are different because the algorithm for the depth and breath first search work
     # differently and there is more than one occurence of 'A' in our graph
+    
     print(bfs.breadth_first_search(graph, 'A'))
     print(dfs.depth_first_search(graph, 'A'))
+
+    print(bfs.breadth_first_search(graph, 'B'))
 
     # As long as there is only one occurence in the graph, results are the same
     print(bfs.breadth_first_search(graph, 'F'))
@@ -89,4 +93,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
